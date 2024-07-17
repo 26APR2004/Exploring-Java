8298f2e6e4ea9d9bd803cc3a78cf3121e0e385c1
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-17 12:47:12 +0530

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,9 @@
+class cmdargs
+{
+ static public void main(String[]args)
+{
+int date=Integer.parseInt(args[1]);
+int month=Integer.parseInt(args[2]);
+int year=Integer.parseInt(args[3]);
+System.out.println(+date+month+year);
+}}
