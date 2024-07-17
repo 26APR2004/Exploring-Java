e3a186d9556bc86abe492d0ef91d5ac83a26f26e
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-17 12:47:34 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+class evenodd
+{
+public static void main (String []args)
+{
+int num=4;
+if(num%2==0)
+{
+System.out.println("number is even");
+}
+else
+{
+System.out.println("number is odd");
+}
+}
+}
+
