40e7031612baec38f783494d9fb2ffdc8d2f6e54
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-17 12:46:47 +0530

    Add files via upload

---
+++ cmdargs.java
@@ -0,0 +1,9 @@
+class cmdargs
+{
+ static public void main(String[]args)
+{
+int date=Integer.ParseInt(args[1]);
+int month=Integer.ParseInt(args[2]);
+int year=Integer.ParseInt(args[3]);
+System.out.println(+date+month+year);
+}}
