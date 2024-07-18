4c6ab58a453b1ea57fe94b39ce44e31f180c69f7
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-18 06:33:45 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,10 @@
+public class ternary
+{
+    static public void main(String[]args)
+    {
+        int a=2;
+        int b=5;
+        int min=(a>b)?a:b;//Ternary operation
+        System.out.println(min);
+    }
+}
