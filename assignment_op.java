32b2a570aa088c9bb528ab725768e25ae67176b3
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-19 05:57:14 +0530

    Add files via upload

---
+++ assignment_op.java
@@ -0,0 +1,15 @@
+class assignment_op
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        a+=3;//a=10+3
+        System.out.println(a);
+        a-=2;//a=13-2
+        System.out.println(a);
+        a*=4;//a=11*4
+        System.out.println(a);
+        a/=11;//a=44/11
+        System.out.println(a);
+    }
+}
