fc0eca7d7964962d781c8088d337ec4a592c6a14
sujal26 <159209658+26APR2004@users.noreply.github.com>
2024-07-20 16:05:45 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class compare
+{
+static public void main(String...args)
+{
+ Scanner w=new Scanner(System.in);
+ System.out.println("Enter the first numeric value:");
+ int a=w.nextInt();
+ System.out.println("Enter the two numeric value:");
+ int b=w.nextInt();
+ if(a>b)
+ {
+System.out.println("a has grater value");
+ }
+else
+{
+System.out.println("b has greater value");
+}
+}
+}
