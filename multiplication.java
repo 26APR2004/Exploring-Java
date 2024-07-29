efd49d0a8b92e0e3a020743c4afb499f8ec3a805
sujal <159209658+26APR2004@users.noreply.github.com>
2024-07-29 21:05:42 +0530

    Create  multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,14 @@
+// java prtogram to display multiplication number
+import java.util.*;
+class multiplication
+{
+    public static void main(String...args)
+    {
+        Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the number which you want multiplication number");
+        int number=sc.nextInt();
+        for(int i=1;i<=10;i++)
+          System.out.println(number*i);
+
+}
+}
