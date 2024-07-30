67b60d822cacdbcb9807690f55ef539c98ff9dff
sujal <159209658+26APR2004@users.noreply.github.com>
2024-07-30 07:43:14 +0530

    Add files via upload

---
+++ Area_triangle.java
@@ -0,0 +1,25 @@
+class Triangl
+{
+    int base;
+    int height;
+
+     void insert(int b;int h)
+     {
+        base=b;
+        height=h;
+     }
+     void display()
+     {
+        Ssytem.out.println("Area of triangle:"+(1/2*base*height);
+     }
+}
+
+public class Area_triangle
+{
+    public static void main(String...args)
+    {
+        Triangle t1=new Triangle();
+        t1.insert(12,4);
+        t1.display();
+    }
+}
