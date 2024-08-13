cdf2d84f5fe5d4752b1292b258cf38945ea00726
sujal <159209658+26APR2004@users.noreply.github.com>
2024-08-13 20:23:11 +0530

    Add files via upload

---
+++ ML1.java
@@ -0,0 +1,21 @@
+interface structureML //interface
+{
+    void ml(String name);
+}
+
+//interface implementation
+class AI implements structureML
+{
+    //Implimentation of abstract class
+    public void ml(String name)
+    {
+        System.out.println("Hello, " + name);
+    }
+}
+
+class ML1{
+    public static void main(String[] args) {
+        AI ai = new AI();
+        ai.ml("John");
+    }
+}
