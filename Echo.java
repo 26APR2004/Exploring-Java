ced67e47375b53f46f26da239410fb5ad72ba869
sujal <159209658+26APR2004@users.noreply.github.com>
2024-08-17 21:05:33 +0530

    Add files via upload

---
+++ Echo.java
@@ -0,0 +1,13 @@
+//java runtime data input
+class hello
+
+{
+    public static void main(String...sujal)
+    {
+        for(int i=0;i<sujal.length;i++)
+        {
+            System.out.println("sujal[i]"+" ");
+        }
+        System.exit(0);
+    }
+}
